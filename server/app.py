2d7452d466c30e995bd737a1904368acce37dccb
Kennedy Musau <kennedy.musauu@gmail.com>
2024-02-28 23:22:06 +0300

    add login resource

--- server/app.py
+++ server/app.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import jsonify, make_response, request, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
 from models import User, Recipe
+
+
+class userById(Resource):
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+        db.session.delete(user)
+        db.session.commit()
+
+        return make_response({"message": "User deleted"}, 200)
 
 
 class Signup(Resource):
@@ -28,7 +37,16 @@ class CheckSession(Resource):
 
 
 class Login(Resource):
-    pass
+    def post(self):
+        username = request.get_json()["username"]
+        password = request.get_json()["password"]
+
+        user = User.query.filter(User.username == username).first()
+        if user.authenticate(password):
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+
+        return make_response({"Invalid username or password"}, 404)
 
 
 class Logout(Resource):
@@ -44,6 +62,8 @@ api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(Logout, '/logout', endpoint='logout')
 api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+
+api.add_resource(userById, "/users/<int:id>")
 
 
 if __name__ == '__main__':
